03456815fb369839f3c75f630045bd4982816514
Tyler Cobb <tyjcobb94@gmail.com>
2020-05-17 16:36:36 -0500

    login endpoint

--- src/functions/AuthHandler.js
+++ src/functions/AuthHandler.js
@@ -15,6 +15,11 @@ const registerSchema = {
     password: Joi.string().required()
 }
 
+const loginSchema = {
+    username: Joi.string().required(),
+    password: Joi.string().required()
+}
+
 const validate = (schema, json) => {
     const {error} = Joi.object().keys(schema).validate(json, {convert: false, stripUnknown: true})
     if (error) {
@@ -28,14 +33,38 @@ module.exports.register = async (event) => {
     try{
         const body = typeof event.body === 'object' ? event.body : JSON.parse(event.body)
         validate(registerSchema, body)
-        const user = await Cognito.signUp({
+        const registerResponse = await Cognito.signUp({
             ClientId: process.env.PUBLIC_USER_POOL_CLIENT_ID,
             Password: body.password,
             Username: body.username
         }).promise()
         return {
             statusCode: 200,
-            body: JSON.stringify(user)
+            body: JSON.stringify(registerResponse)
+        }
+    } catch (e) {
+        return {
+            statusCode: e.statusCode || 500,
+            body: JSON.stringify(e.message)
+        }
+    }
+}
+
+module.exports.login = async (event) => {
+    try{
+        const body = typeof event.body === 'object' ? event.body : JSON.parse(event.body)
+        validate(loginSchema, body)
+        const loginResponse = await Cognito.initiateAuth({
+            AuthFlow: 'USER_PASSWORD_AUTH',
+            ClientId: process.env.PUBLIC_USER_POOL_CLIENT_ID,
+            AuthParameters: {
+                PASSWORD: body.password,
+                USERNAME: body.username
+            },
+        }).promise()
+        return {
+            statusCode: 200,
+            body: JSON.stringify(loginResponse)
         }
     } catch (e) {
         return {

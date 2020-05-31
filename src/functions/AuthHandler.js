d8a5cc6e275b0ab64b5e70f048aa44e56fed3f1b
Tyler Cobb <tyjcobb94@gmail.com>
2020-05-31 12:58:36 -0500

    Allow access token header

--- src/functions/AuthHandler.js
+++ src/functions/AuthHandler.js
@@ -79,7 +79,8 @@ function createResponse(statusCode, body) {
         statusCode: statusCode || 500,
         body: JSON.stringify(body),
         headers: {
-            'Access-Control-Allow-Origin': '*'
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Authorization,AccessToken'
         }
     }
 }

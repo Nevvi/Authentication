3b2d44e969c922362a243dce188369ebe4a03686
Tyler Cobb <tyjcobb94@gmail.com>
2020-06-21 10:44:49 -0500

    Use shared user http client with api auth logic

--- src/dao/user/UserDao.js
+++ src/dao/user/UserDao.js
@@ -1,13 +1,23 @@
 'use strict'
 
-const axios = require('axios')
+const UserHttpClient = require('../../../Common/http/user/UserHttpClient')
 
 module.exports = class UserDao {
     constructor() {
-        this.baseUrl = process.env.USER_API_URL
+        this.client = new UserHttpClient(
+            process.env.USER_API_URL,
+            process.env.API_CLIENT_ID,
+            process.env.API_CLIENT_SECRET,
+            process.env.USER_API_SCOPES,
+        )
     }
 
     async createUser(username) {
-        return await axios.post(`${this.baseUrl}/v1/users`, {username})
+        try {
+            return await this.client.createUser({username})
+        } catch (e) {
+            console.log(`Failed to create user`, e)
+            throw e
+        }
     }
 }
